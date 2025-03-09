5774654c8459b3b05c570458fccbadc8f0ffa06c
Emmanuel <nuelchimezie404@gmail.com>
2025-03-09 11:44:43 +0100

    update

---
+++ test.js
@@ -0,0 +1,25 @@
+let btn = document.getElementById('submit')
+let input = document.getElementById('input')
+let resultEl = document.getElementById('resultEl')
+
+let optionCont = document.getElementById('optionEl')
+
+let option1 = document.getElementById('option1')
+let option2 = document.getElementById('option2')
+
+
+
+function convertFeet() {
+  let inputNum = input.value;
+  console.log(inputNum)
+  let times = 30.48;
+  let results = inputNum*times
+  console.log(results)
+  resultEl.innerHTML = results+ 'cm'
+  
+ input.value=''
+}
+
+
+
+
